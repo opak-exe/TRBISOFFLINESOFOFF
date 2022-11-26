025630abdef614a0890ec506fa529efe4fbb2672
opak-exe <zriguiadam@gmail.com>
2022-11-26 23:38:50 +0100

    69 me if u can

---
+++ src/Hello.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+const Hello = () => {
+  return (
+    <>
+        <p >Good morning USA
+I got a feeling that it's gonna be a wonderful day
+The sun in the sky has a smile on his face
+And he's shining a salute to the American race
+Oh, boy, it's swell to say
+Good morning USA!
+(Good morning USA!)</p>
+    </>
+  )
+}
+
+export default Hello
---
+++ src/Name.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Name = props => {
+
+  return (
+    <>
+<h2>My name is {props.name}</h2>
+    </>
+  )
+}
+
+export default Name
